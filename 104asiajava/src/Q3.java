bc371bae3cac88bca7b3672c5d6181ef51545c6d
asdasd950 <o60417o@yahoo.com.tw>
2015-12-07 16:05:20 +0800

    2.0

--- 104asiajava/src/Q3.java
+++ 104asiajava/src/Q3.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 
 public class Q3 {
 
@@ -6,7 +8,19 @@ public class Q3 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn=new Scanner(System.in);
+		int x=scn.nextInt();
+		gg(x);
 	}
-
+	public static void gg(int m){
+		int y=0;
+		if(m<-1){
+			y=(int)(3*(Math.pow(m,2)));
+		}else if(m>=-1&&m<=1){
+			y=(int)Math.pow(m,3)+3*m-3;
+		}else if(m>1){
+			y=2*m+3;
+		}
+		System.out.println(y);
+	}
 }
